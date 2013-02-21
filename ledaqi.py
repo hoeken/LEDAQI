fa5a8361f0ddf7d8c16f6efd1507aafda13b2c5d
Zach Hoeken <zach@makerbot.com>
2013-02-21 22:18:27 +0800

    updated code a bit more.

--- ledaqi.py
+++ ledaqi.py
@@ -5,12 +5,13 @@ from bs4 import BeautifulSoup
 import Image
 import base64
 import tempfile
+import serial.tools.list_ports
 
 class blinkyBoard:
   def init(self, port, baud):
     self.serial = serial.Serial(port, baud)
     self.rgamma = 2.0
-    self.ggamma = 2.0
+    self.ggamma = 1.0
     self.bgamma = 4.0
 
   def sendPixel(self,g,r,b):
@@ -36,12 +37,17 @@ class blinkyBoard:
     self.serial.write(data)
     self.serial.flush()
 
-blink = blinkyBoard()
-blink.init('/dev/cu.usbmodemfa131', 57600)
+ports = serial.tools.list_ports.comports();
+port = ports[0][0]
 
-url = "http://www.aqicn.info/?city=Shenzhen"
-#url = "http://www.aqicn.info/?city=Beijing"
-#url = "http://www.aqicn.info/?city=Shanghai"
+blink = blinkyBoard()
+blink.init(port, 57600)
+
+#url = "http://www.aqicn.info/city/hongkong/"
+#url = "http://www.aqicn.info/city/shenzhen/"
+url = "http://www.aqicn.info/city/shenzhen/huaqiaocheng/"
+#url = "http://www.aqicn.info/city/beijing/"
+#url = "http://www.aqicn.info/city/shanghai/"
 #url = "http://www.aqicn.info/"
 
 positions = [243, 238, 233, 228, 223, 218, 213, 208, 203, 198, 193, 188, 183, 178, 173, 168, 163, 158, 153, 148, 143, 138, 133, 128, 123, 118, 113]
@@ -50,32 +56,58 @@ for i in range(0, 30):
   blink.sendPixel(0,0,0);
 blink.sendBreak()
 
-while True:
-  print "Scraping %s" % url
-  soup = BeautifulSoup(urllib.urlopen(url), "html5lib")
-  imgs = soup.find_all(id="img_pm25")
-  #print imgs
-  if len(imgs):
-    imgurl = imgs[0]['src']
+try:
+  while True:
+    try:
+      print "[%d] Scraping %s" % (time.time(), url)
+      data = urllib.urlopen(url)
+      soup = BeautifulSoup(data, "html5lib")
+      imgs = soup.find_all(id="img_pm25")
+      #print imgs
+      if len(imgs):
+        imgurl = imgs[0]['src']
   
-    #print "Parsing image"
-    imgdata = base64.b64decode(imgurl[22:])
-    tmp = open("data.png", "w")
-    tmp.write(imgdata)
-    tmp.close()
-    img = Image.open("data.png")
-    img = img.convert('RGB')
+        #print "Parsing image"
+        imgdata = base64.b64decode(imgurl[22:])
+        tmp = open("data.png", "w")
+        tmp.write(imgdata)
+        tmp.close()
+        img = Image.open("data.png")
+        img = img.convert('RGB')
+        
+        end = time.time() + 60*30;
 
-    #print "Points"
-    for x in positions:
-      r, g, b = img.getpixel((x, 24))
-      #print r, g, b
-      blink.sendPixel(r, g, b);
-    blink.sendBreak()
-    time.sleep(60*30)
-  else:
-    print "Site down."
-    time.sleep(60)
+        delay = 0.75
+        yheight = 25
 
-  pass
-  
+        while (time.time() < end):
+
+          for x in positions:
+            r, g, b = img.getpixel((x, yheight))
+            #print r, g, b
+            blink.sendPixel(r, g, b);
+          blink.sendBreak()
+
+          time.sleep(delay)
+
+          for idx, x in enumerate(positions):
+            r, g, b = img.getpixel((x, yheight))
+            if idx == 0:
+              r = 0
+              g = 0
+              b = 0
+
+            blink.sendPixel(r, g, b);
+          blink.sendBreak()
+          
+          time.sleep(delay)
+        # time.sleep(60*30)
+      else:
+        print "Site down."
+        time.sleep(60)
+    except KeyboardInterrupt as ex:
+      raise ex
+    except Exception as ex:
+      print ex
+except KeyboardInterrupt as ex:
+  print "Exiting."
